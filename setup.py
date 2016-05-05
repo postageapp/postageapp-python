fa72c502c5095f0e8840900f3dce19e28ad58f5c
Scott Tadman <scott@twg.ca>
2016-05-05 01:57:16 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='postageapp',
-    version='0.1.0',
+    version='0.1.1',
     description='Library for sending email via the PostageApp service',
     url='http://github.com/postageapp/postageapp-python',
     author='Scott Tadman',
